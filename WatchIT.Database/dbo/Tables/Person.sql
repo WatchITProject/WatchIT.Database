8bd1820a4bf01091287b053d3e7ef4f171cdeb5b
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-09 23:03:44 +0100

    Person table added

---
+++ WatchIT.Database/dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[Id]			INT				NOT NULL	IDENTITY(1,1),
+	[Name]			VARCHAR(100)	NOT NULL,
+	[FullName]		VARCHAR(200)	NULL,
+	[Description]	VARCHAR(MAX)	NULL,
+	[Birthday]		DATE			NULL,
+	CONSTRAINT PK_Person PRIMARY KEY (Id)
+)
