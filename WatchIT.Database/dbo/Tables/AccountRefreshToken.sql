a0b1265873faeb82b3cb854dafeb825f5cbc9db3
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-18 13:10:45 +0100

    AccountRefreshToken table added

---
+++ WatchIT.Database/dbo/Tables/AccountRefreshToken.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AccountRefreshToken]
+(
+	[Id]			UNIQUEIDENTIFIER	NOT NULL,
+	[AccountId]		INT					NOT NULL,
+	[Key]			VARCHAR(MAX)		NOT NULL,
+	[Extendable]	BIT					NOT NULL	DEFAULT(0),
+	CONSTRAINT PK_AccountRefreshToken PRIMARY KEY (Id),
+	CONSTRAINT FK_AccountRefreshToken_Account FOREIGN KEY (AccountId) REFERENCES Account(Id)
+)
