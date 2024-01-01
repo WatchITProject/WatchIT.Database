932e03689498dc16f0384503f9af055b3c03257e
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-01-01 19:36:34 +0100

    PosterImageContentType

--- WatchIT.Database/dbo/Tables/Media.sql
+++ WatchIT.Database/dbo/Tables/Media.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[Media]
 (
-	[Id]			BIGINT			NOT NULL	IDENTITY(1,1),
-	[Title]			VARCHAR(400)	NOT NULL,
-	[OriginalTitle]	VARCHAR(400)	NULL,
-	[Description]	VARCHAR(MAX)	NULL,
-	[ReleaseDate]	DATE			NULL,
-	[Length]		SMALLINT		NULL,
-	[PosterImage]	VARBINARY(MAX)	NULL,
+	[Id]						BIGINT			NOT NULL	IDENTITY(1,1),
+	[Title]						VARCHAR(400)	NOT NULL,
+	[OriginalTitle]				VARCHAR(400)	NULL,
+	[Description]				VARCHAR(MAX)	NULL,
+	[ReleaseDate]				DATE			NULL,
+	[Length]					SMALLINT		NULL,
+	[PosterImage]				VARBINARY(MAX)	NULL,
+	[PosterImageContentType]	VARCHAR(100)	NULL,
 	CONSTRAINT PK_Media PRIMARY KEY (Id)
 )
